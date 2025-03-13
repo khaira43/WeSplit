0a6b8c825a203e2afd742a7f28125fc958e2c95c
Ranbir Singh Khaira <rkhaira1010@gmail.com>
2025-03-13 11:37:45 -0400

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ranbir Khaira on 2025-03-12.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
